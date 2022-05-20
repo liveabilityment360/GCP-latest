d8749ecf444bedf5f308ee8fbcbc8c2c19fa5cbd
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-05-20 18:47:58 +1000

    Create install_packages.sh
    Installations required for new VM instance

---
+++ Scripts/install_packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update 
+sudo apt-get install git-core
+sudo apt install python3-pip
+sudo pip install google-cloud
+sudo pip install google-cloud-pubsub
+python3 -m pip install --upgrade pip
