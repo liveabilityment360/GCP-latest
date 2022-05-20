64a83f738d7d65acb18e0e1def981926a2042998
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-05-20 18:54:32 +1000

    Update install_packages.sh
    Set up the Apache2 HTTP Server.

--- Scripts/install_packages.sh
+++ Scripts/install_packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update 
 sudo apt-get install git-core
+sudo apt-get install apache2 php7.0
 sudo apt install python3-pip
 sudo pip install google-cloud
 sudo pip install google-cloud-pubsub
