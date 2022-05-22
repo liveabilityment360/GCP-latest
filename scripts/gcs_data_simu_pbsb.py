ea9e882b4232b2992e473f2edfe127b0e3380948
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-05-22 20:32:39 +1000

    Update gcs_data_simu_pbsb.py

--- scripts/gcs_data_simu_pbsb.py
+++ scripts/gcs_data_simu_pbsb.py
@@ -11,7 +11,7 @@ from google.cloud import pubsub
 from csv import reader
 from google.cloud import storage
 dir = os.getcwd()
-bucket_name='gs://private-equity/pe_data/raw_pe_data.csv'
+bucket_name='gs://private_equity/pe_data/raw_pe_data.csv'
 os.system('gsutil cp '+ bucket_name  +' '+ dir)
 data_file = os.path.join(dir,'raw_pe_data.csv')
 
