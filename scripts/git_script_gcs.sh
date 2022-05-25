fc30943828186ef4aa5431144528af60fee63b07
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-05-25 20:13:18 +1000

    Create git_script_gcs.sh

---
+++ scripts/git_script_gcs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd
+gsutil cp 'finfo/scripts/git_data_gcs.sh' gs://australia-southeast1-compos-ca616f74-bucket
+gsutil cp 'finfo/scripts/gcs_df_bq.py' gs://australia-southeast1-compos-ca616f74-bucket
