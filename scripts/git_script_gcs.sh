74854fcce832a3242f1e7c4ab97adf47957b6659
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-05-25 20:16:01 +1000

    Update git_script_gcs.sh

--- scripts/git_script_gcs.sh
+++ scripts/git_script_gcs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd
-gsutil cp 'finfo/scripts/git_data_gcs.sh' gs://australia-southeast1-compos-ca616f74-bucket
-gsutil cp 'finfo/scripts/gcs_df_bq.py' gs://australia-southeast1-compos-ca616f74-bucket
+gsutil cp 'finfo/scripts/git_data_gcs.sh' gs://australia-southeast1-compos-ca616f74-bucket/dags
+gsutil cp 'finfo/scripts/gcs_df_bq.py' gs://australia-southeast1-compos-ca616f74-bucket/dags
