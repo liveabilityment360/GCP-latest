d9ea3c4aec07c71596d79d1b114348d8e18fa150
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-05-26 20:03:12 +1000

    Update git_script_gcs.sh

--- scripts/git_script_gcs.sh
+++ scripts/git_script_gcs.sh
@@ -1,7 +1,7 @@
 
 
 # copying scripts for cloud composer in dags folder
-gsutil cp 'finfo/scripts/git_data_gcs.sh' gs://australia-southeast1-compos-ca616f74-bucket/dags
-gsutil cp 'finfo/scripts/gcs_data_simu_pbsb.py' gs://australia-southeast1-compos-ca616f74-bucket/dags
-gsutil cp 'finfo/scripts/pe_pb_df_bq.py' gs://australia-southeast1-compos-ca616f74-bucket/dags
+gsutil cp 'finfo/scripts/git_data_gcs.sh'  gs://australia-southeast1-cloudc-61378e87-bucket/dags
+gsutil cp 'finfo/scripts/gcs_data_simu_pbsb.py' gs://australia-southeast1-cloudc-61378e87-bucket/dags
+gsutil cp 'finfo/scripts/pe_pb_df_bq.py'  gs://australia-southeast1-cloudc-61378e87-bucket/dags
 
