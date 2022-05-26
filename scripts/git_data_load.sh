2006c822b2180ddeb193c80f0885438492d28eb7
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-05-27 01:16:53 +1000

    Update git_data_load.sh

--- scripts/git_data_load.sh
+++ scripts/git_data_load.sh
@@ -5,11 +5,12 @@ gsutil cp 'finfo/data/raw_pe_tdata.csv' gs://private_equity
 gsutil cp 'finfo/data/dim_date.csv' gs://private_equity
 
 # loading dim_date into date dimesion table.
+  
     bq load \
+ --autodetect \
     --source_format=CSV \
     private_equity.dim_date\
-    gs://private_equity/dim_date1.csv \
-    year:INTEGER,month:INTEGER,day:INTEGER,date:DATE,date_id:INTEGER,date_name:DATE,fiscal_year:INTEGER,fiscal_quarter:INTEGER,calendar_quarter:INTEGER,is_week_day:BOOLEAN,day_of_week:INTEGER,month_name:STRING,day_of_week_name:STRING
+    gs://private_equity/pe_data_dim_date.csv
 
 # copying scripts for cloud composer in dags folder
 gsutil cp 'finfo/scripts/git_data_gcs.sh' gs://australia-southeast1-compos-ca616f74-bucket/dags
