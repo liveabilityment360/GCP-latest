cf40a358d8c2904527312897d205b0733d79599f
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-05-27 01:05:26 +1000

    Update git_data_gcs.sh

--- scripts/git_data_gcs.sh
+++ scripts/git_data_gcs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd
 # copying raw data and dim_date csv files in GCS bucket.
-gsutil cp 'finfo/data/raw_pe_tdata.csv' gs://private_equity/pe_data
-
+gsutil cp 'finfo/data/raw_pe_tdata.csv' gs://private_equity
