7dde1450e0ace193c0799bcf6d7787b201443d25
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-05-26 17:00:35 +1000

    Update git_data_gcs.sh

--- scripts/git_data_gcs.sh
+++ scripts/git_data_gcs.sh
@@ -2,3 +2,5 @@
 cd
 gsutil cp 'finfo/data/raw_pe_tdata.csv' gs://private_equity/pe_data
 gsutil cp 'finfo/data/dim_date.csv' gs://private_equity/pe_data
+gsutil cp 'finfo/scripts/git_data_gcs.sh' gs://australia-southeast1-compos-ca616f74-bucket/dags
+gsutil cp 'finfo/scripts/gcs_df_bq.py' gs://australia-southeast1-compos-ca616f74-bucket/dags
