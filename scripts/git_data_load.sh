95045fe1730dd110c8618f03e88cb3c45e024fcc
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-05-27 01:10:05 +1000

    Update git_data_load.sh

--- scripts/git_data_load.sh
+++ scripts/git_data_load.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd
 # copying raw data and dim_date csv files in GCS bucket.
-gsutil cp 'finfo/data/raw_pe_tdata.csv' gs://private_equity/pe_data
-gsutil cp 'finfo/data/dim_date.csv' gs://private_equity/pe_data
+gsutil cp 'finfo/data/raw_pe_tdata.csv' gs://private_equity
+gsutil cp 'finfo/data/dim_date.csv' gs://private_equity
 
 # loading dim_date into date dimesion table.
     bq load \
     --source_format=CSV \
     private_equity.dim_date\
-    gs://private_equity/pe_data/dim_date1.csv \
+    gs://private_equity/dim_date1.csv \
     year:INTEGER,month:INTEGER,day:INTEGER,date:DATE,date_id:INTEGER,date_name:DATE,fiscal_year:INTEGER,fiscal_quarter:INTEGER,calendar_quarter:INTEGER,is_week_day:BOOLEAN,day_of_week:INTEGER,month_name:STRING,day_of_week_name:STRING
 
 # copying scripts for cloud composer in dags folder
