75daa81a6545859aa086368b9f7f62c9ceba1b29
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-05-28 15:12:24 +1000

    Update create_table.sh

--- ddl/create_table.sh
+++ ddl/create_table.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bq --location= $location mk -d cloud-build-test-351109:private_equity
+bq --location= $location mk -d gcp-project-346311:private_equity
 bq mk   -t   --description "FINFO landing table"   \private_equity.raw_priv_equi  timestamp:TIMESTAMP,company_name:STRING,growth_stage:STRING,country:STRING,state:STRING,city:STRING,continent:STRING,industry:STRING,sub_industry:STRING,client_focus:STRING,business_model:STRING,company_status:STRING,round:STRING,amount_raised:STRING,currency:STRING,date:STRING,quarter:STRING,month:STRING,year:STRING,investor_types:STRING,investor_name:STRING,company_valuation_usd:STRING,valuation_date:STRING
