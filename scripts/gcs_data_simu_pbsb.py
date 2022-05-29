07cd0838bfb823801aebdbdad1c815735bae8921
Gitnoob007 <98385295+Gitnoob007@users.noreply.github.com>
2022-05-29 17:01:16 +1000

    Update gcs_data_simu_pbsb.py

--- scripts/gcs_data_simu_pbsb.py
+++ scripts/gcs_data_simu_pbsb.py
@@ -11,7 +11,7 @@ from google.cloud import pubsub
 from csv import reader
 from google.cloud import storage
 dir = os.getcwd()
-bucket_name='gs://private_equity/raw_pe_tdata.csv'
+bucket_name='gs://gcp-project-346311/raw_pe_tdata.csv'
 os.system('gsutil cp '+ bucket_name  +' '+ dir)
 data_file = os.path.join(dir,'raw_pe_tdata.csv')
 
