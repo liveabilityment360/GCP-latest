0184e2efe550cd5502e12ad890ea4105958e4ecc
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-05-29 16:38:32 +1000

    Update git_data_load.sh

--- scripts/git_data_load.sh
+++ scripts/git_data_load.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+projectid=$1
+dagpath=$2
 cd
 # copying raw data and dim_date csv files in GCS bucket.
-gsutil cp 'finfo/data/raw_pe_tdata.csv' gs://private_equity
-gsutil cp 'finfo/data/dim_date.csv' gs://private_equity
-#gsutil cp 'finfo/data/dbtk.json' gs://private_equity
+gsutil cp 'finfo/data/raw_pe_tdata.csv' gs://$(gcloud config list --format 'value(core.project)' 2>/dev/null)
+gsutil cp 'finfo/data/dim_date.csv' gs://$(gcloud config list --format 'value(core.project)' 2>/dev/null)
+#gsutil cp 'finfo/data/dbtk.json' gs://$(gcloud config list --format 'value(core.project)' 2>/dev/null)
 # loading dim_date into date dimesion table.
   
     bq load \
  --autodetect \
     --source_format=CSV \
     private_equity.dim_date\
-    gs://private_equity/dim_date.csv
+    gs://gcp-project-346311/dim_date.csv
 
 # copying scripts for cloud composer in dags folder
-#gsutil cp 'finfo/scripts/git_data_gcs.sh' gs://australia-southeast1-compos-ca616f74-bucket/dags
-#gsutil cp 'finfo/scripts/gcs_df_bq.py' gs://australia-southeast1-compos-ca616f74-bucket/dags
+gsutil cp 'finfo/scripts/git_data_gcs.sh'  gs://australia-southeast1-finc3-f8af926e-bucket/dags
+gsutil cp 'finfo/scripts/gcs_df_bq.py' gs://australia-southeast1-finc3-f8af926e-bucket/dags
 
 
