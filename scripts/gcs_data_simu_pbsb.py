23ceb146748ed3438f150d87d045551d43ed1e94
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-06-02 19:19:02 +1000

    Update gcs_data_simu_pbsb.py

--- scripts/gcs_data_simu_pbsb.py
+++ scripts/gcs_data_simu_pbsb.py
@@ -39,7 +39,7 @@ def simulate(topic, ifp, firstObsTime, programStart):
        for line in ifp:
          topublish = list()
          event_data = json.dumps(line)
-         to_sleep_secs = random.uniform(1,3)
+         to_sleep_secs = random.uniform(0.4,0.7)
          logging.info('Sleeping {} seconds'.format(to_sleep_secs))
          time.sleep(to_sleep_secs)
          topublish.append(event_data)
