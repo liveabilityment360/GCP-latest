5284f1184684b3849d4486fe3c7313a91421fb8d
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-06-02 23:17:47 +1000

    Update create_table.sh

--- ddl/create_table.sh
+++ ddl/create_table.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 bq --location=australia-southeast2 mk -d gcp-project-346311:private_equity
+
 bq mk   -t   --description "FINFO landing table"   \private_equity.raw_priv_equi  rec_crt_ts:TIMESTAMP,company_name:STRING,growth_stage:STRING,country:STRING,state:STRING,city:STRING,continent:STRING,industry:STRING,sub_industry:STRING,client_focus:STRING,business_model:STRING,company_status:STRING,round:STRING,amount_raised:STRING,currency:STRING,date:STRING,quarter:STRING,Month:STRING,Year:STRING,investor_types:STRING,investor_name:STRING,company_valuation_usd:STRING,valuation_date:STRING
 # create partitioned table for fact_pe
+bq mk -t \
+  --schema 'country_id:BYTES,comapny_id:BYTES,investor_id:BYTES,date_id:INTEGER,rcd_date:DATE,funding_rnd:STRING,amount_raised:INTEGER,currency:STRING, unique_id:STRING, current_timestamp:TIMESTAMP' \
+  --time_partitioning_field rcd_date \
+  --time_partitioning_type DAY \
+  private_equity.fact_pe
+  
 bq mkdef --source_format=CSV --autodetect=TRUE \
   gs://gcp-project-346311/dim_date.csv > dim_date_df
  
@@ -11,11 +18,7 @@ bq mk --table \
 
 rm -f dim_date_df
 
-bq mk -t \
-  --schema 'country_id:BYTES,comapny_id:BYTES,investor_id:BYTES,date_id:INTEGER,rcd_date:DATE,funding_rnd:STRING,amount_raised:INTEGER,currency:STRING, unique_id:STRING, current_timestamp:TIMESTAMP' \
-  --time_partitioning_field rcd_date \
-  --time_partitioning_type DAY \
-  private_equity.fact_pe
+
 #create dim_investor table
 bq --location= $location mk -d gcp-project-346311:private_equity
 bq mk   -t   --description "FINFO investor dimension"   \private_equity.dim_investor  investor_name:STRING,investor_types:STRING,investor_id:BYTES,rec_crt_ts:TIMESTAMP
@@ -34,7 +37,7 @@ bq mk \
 --expiration 0 \
 --description "PE view" \
 --view \
-select d.year as Year,
+'select d.year as Year,
 d.month as Month,
 c.company_name as Company, 
 c.company_valuation as CompanyValuation,
@@ -56,6 +59,6 @@ where f.date_id = d.date_id
 and f.company_id = c.company_id
 and f.country_id = a.country_id
 group by d.year,d.Month, c.company_name,a.country,c.company_valuation,c.industry,c.sub_industry,c.growth_stage,b.investor_name,b.investor_types, a.state,a.city,f.amount_raised
-Order by AmountRaised;
+Order by AmountRaised'\
 private_equity.pe_view
 
