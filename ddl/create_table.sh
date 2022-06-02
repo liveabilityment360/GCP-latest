9e289be7acec3a82d4f9cc529e4e37d1cb5465af
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-06-02 23:19:56 +1000

    Update create_table.sh

--- ddl/create_table.sh
+++ ddl/create_table.sh
@@ -18,19 +18,14 @@ bq mk --table \
 
 rm -f dim_date_df
 
-
 #create dim_investor table
-bq --location= $location mk -d gcp-project-346311:private_equity
 bq mk   -t   --description "FINFO investor dimension"   \private_equity.dim_investor  investor_name:STRING,investor_types:STRING,investor_id:BYTES,rec_crt_ts:TIMESTAMP
 
 #create dim_country table
-bq --location= $location mk -d gcp-project-346311:private_equity
 bq mk   -t   --description "FINFO country dimension"   \private_equity.dim_country  country:STRING,state:STRING,city:STRING,continent:STRING,country_id:BYTES,rec_crt_ts:TIMESTAMP
 
 #create dim_company table
-bq --location= $location mk -d gcp-project-346311:private_equity
 bq mk   -t   --description "FINFO company dimension"   \private_equity.dim_company  company_name:STRING,growth_stage:STRING,industry:STRING,sub_industry:STRING,client_focus:STRING,business_model:STRING,company_status:STRING,company_valuation:STRING,valuation_date:DATE,company_id:BYTES,rec_crt_ts:TIMESTAMP
-  
   
 bq mk \
 --use_legacy_sql=false \
