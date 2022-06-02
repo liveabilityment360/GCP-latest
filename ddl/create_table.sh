66549bf5a6a1edaaf4f461f09b2b30c3d97ec453
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-06-03 00:28:08 +1000

    Update create_table.sh

--- ddl/create_table.sh
+++ ddl/create_table.sh
@@ -27,4 +27,33 @@ bq mk   -t   --description "FINFO country dimension"   \private_equity.dim_count
 #create dim_company table
 bq mk   -t   --description "FINFO company dimension"   \private_equity.dim_company  company_name:STRING,growth_stage:STRING,industry:STRING,sub_industry:STRING,client_focus:STRING,business_model:STRING,company_status:STRING,company_valuation:STRING,valuation_date:DATE,company_id:BYTES,rec_crt_ts:TIMESTAMP
   
-
+bq mk \
+--use_legacy_sql=false \
+--expiration 0 \
+--description "PE view" \
+--view \
+'select d.year as Year,
+d.month as Month,
+c.company_name as Company,
+c.company_valuation as CompanyValuation,
+c.industry as Industry,
+c.sub_industry as SubIndustry,
+c.growth_stage as GrowthStage,
+a.country as Country,
+a.state as State,
+a.city as City,
+b.investor_name as InvestorName,
+b.investor_types as InvestorType,
+f.amount_raised as AmountRaised
+from `gcp-project-346311.private_equity.dim_company`c,
+`gcp-project-346311.private_equity.dim_country`a,
+`gcp-project-346311.private_equity.dim_date` d,
+`gcp-project-346311.private_equity.dim_investor`b,
+`gcp-project-346311.private_equity.fact_pe` f
+where f.date_id = d.date_id
+and f.company_id = c.company_id
+and f.country_id = a.country_id
+and f.investor_id = b.investor_id
+group by d.year,d.Month, c.company_name,a.country,c.company_valuation,c.industry,c.sub_industry,c.growth_stage,b.investor_name,b.investor_types, a.state,a.city,f.amount_raised
+Order by AmountRaised' \
+private_equity.pe_view
