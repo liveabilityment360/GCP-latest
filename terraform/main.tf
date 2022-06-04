dbdb543fa44c981969e1f763875c74330acfff76
FINFO-GCP-Project <105575396+FINFO-GCP-Project@users.noreply.github.com>
2022-06-04 21:30:14 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,10 +48,12 @@ resource "google_compute_instance" "default" {
     }
   }
 
-
 # Install setup files
   #metadata_startup_script = "sudo apt-get update;sudo apt-get install git-core;sudo apt-get install apache2 php7.0;sudo apt install python3-pip;sudo pip install google-cloud;sudo pip install google-cloud-pubsub;python3 -m pip install --upgrade pip;pip install apache-beam;pip install apitools;pip install api-base;pip install --upgrade google-cloud-storage"
-  metadata_startup_script="echo hi "> /install.txt
+  metadata_startup_script = <<-EOF
+  echo "hello
+  you" > /install.txt
+  EOF
   network_interface {
     network = "default"
 
