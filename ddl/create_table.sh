474747f1a844dcbaa56ca2e3585be47100d9c040
PratibhaGangavarapu <106134746+PratibhaGangavarapu@users.noreply.github.com>
2022-06-05 13:02:25 +1000

    Update create_table.sh

--- ddl/create_table.sh
+++ ddl/create_table.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bq --location=us-central1 mk -d striped-impulse-352211:private_equity
+bq --location=australia-southeast2-a mk -d striped-impulse-352211:private_equity
 
 bq mk   -t   --description "FINFO landing table"   \private_equity.raw_priv_equi  rec_crt_ts:TIMESTAMP,company_name:STRING,growth_stage:STRING,country:STRING,state:STRING,city:STRING,continent:STRING,industry:STRING,sub_industry:STRING,client_focus:STRING,business_model:STRING,company_status:STRING,round:STRING,amount_raised:STRING,currency:STRING,date:STRING,quarter:STRING,Month:STRING,Year:STRING,investor_types:STRING,investor_name:STRING,company_valuation_usd:STRING,valuation_date:STRING
 # create partitioned table for fact_pe
